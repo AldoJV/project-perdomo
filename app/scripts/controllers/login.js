bfca7eabd8187fa7ccf717c0f5a6fd7c225912a7
Aldo <AldoJV@github.com>
2016-04-01 18:56:45 -0600

    login form sketch

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,8 +11,12 @@ angular.module('yapp')
   .controller('LoginCtrl', function($scope, $location) {
 
     $scope.submit = function() {
-
-      $location.path('/dashboard');
+    	if ($scope.form.$valid) {
+    		console.log('VALID');
+    	}else{
+    		console.log('Invalid');
+    	}
+      //$location.path('/dashboard');
 
       return false;
     }
